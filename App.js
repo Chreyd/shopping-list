1e4e43d42022be4e4af49820adbd082ddb07c2ae
Fleudry <chreydmapeba@gmail.com>
2024-01-25 14:13:55 +0100

    Application Shopping list partie 2

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
-  return <View style={styles.container}>
-    <Text>Hello word</Text>
-  </View>;
+  const [product, setProduct] = useState("");
+  const [myProducts, setMyProducts] = useState([]);
+
+  const inputHandler = (val) => {
+    setProduct(val);
+  };
+
+  const submitHandler = () => {
+    console.log(product);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Nouveau Produit"
+          // onChangeText={(e)=>inputHandler(e)}
+          onChangeText={inputHandler}
+          value={product}
+        />
+        <Button title="Valider" onPress={submitHandler} />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-
+    padding: 40,
+    paddingTop: 60,
+  },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "grey",
+    padding: 5,
+    paddingLeft: 9,
+    fontSize: 18,
+    flexGrow: 1,
   },
 });
